6379d89d87ca1234a9cacb4f10be27707a1dcfbe
KarikariAdade <juniorlecrae@gmail.com>
2021-08-13 13:38:13 +0000

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,103 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:splash_screen/homepage.dart';
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(primaryColor: Colors.blueAccent, accentColor: Colors.yellowAccent),
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen(),
+  routes: {
+    '/homepage': (context) => HomePage()
+  }
+));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () => Navigator.pushNamed(context, '/homepage'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blueAccent
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                            Icons.shopping_cart,
+                          color: Colors.greenAccent,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        'BizCart',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // Circular Progress Indicator is a loading circular Indicator
+                    CircularProgressIndicator(
+                      backgroundColor: Colors.white,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0)
+                    ),
+                    Text(
+                      'Online Store \nFor Everyone',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
